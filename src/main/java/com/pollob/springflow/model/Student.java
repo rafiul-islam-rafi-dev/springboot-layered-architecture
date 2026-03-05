8cc763589d641afbedaab14f92018299a376f646
rafiul-islam-rafi-dev <rafiulislam1841@gmail.com>
2026-03-05 23:50:09 +0600

    docs: add detailed comments for better understanding

--- src/main/java/com/pollob/springflow/model/Student.java
+++ src/main/java/com/pollob/springflow/model/Student.java
@@ -1,13 +1,27 @@
 // ei class "database table" represent kore.
 package com.pollob.springflow.model;
 
+// JPA(Java Persistence API) er annotation use korar jonno. ex: Entity, Id, Table
 import jakarta.persistence.*;
 
+// Eta Spring k bole ei class DatabaseTable hobe. Jodi ei annotation na dei tahole table create hobe na.
 @Entity
-public class Student {
+// "@Entity" thakai ei class akhon Database Entity.
+public class Student { 
 
+	// "Id" bujhai ei field ta primary key hobe.
     @Id
+     /*
+     * Ei annotation mane value automatically generate hobe.
+     * "strategy" bujhai kon rules a value generate hobe.
+     * "GenerationType.IDENTITY" eit bujhai Database nije Id generate korbe, "Hibernate/JPA" er responsibility na.
+     */
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+   
+    /* 
+     * Ei variable bahire theke access kora jabe na, etai Encapsulation.
+     * "Long" dilam jate id=null jeno hoy, id=0 na hoy. karon id=0 hole spring bujhte pare na id "0" new object naki id=0.
+     */
     private Long id;
 
     private String name;
@@ -18,6 +32,11 @@ public class Student {
         return id;
     }
 
+    /*
+     * Ei method String return korbe.
+     * agei setter method use kore Student-class er "name" variable a value set kore rakhchi.
+     * Student class er "name" variable er sei value return korchi.
+     */
     public String getName() {
         return name;
     }
@@ -31,6 +50,12 @@ public class Student {
         this.id = id;
     }
 
+    /*
+     * value set korchi tai kono return type korbo na tai "void" use korlam.
+     * String name: "String" data-Type and "name" parameter
+     * "this.name" holo student class er variable.
+     * paramerer a asa "name" value, student class er "name" variable a set/store hobe.
+     */
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/com/pollob/springflow/service/StudentService.java
+++ src/main/java/com/pollob/springflow/service/StudentService.java
@@ -4,9 +4,21 @@ package com.pollob.springflow.service;
 import java.util.List;
 import com.pollob.springflow.model.Student;
 
+/*
+ * Interface karon:
+ * akhane sudu "ki kaj hobe" seta declare kori
+ * "kivabe kaj hobe" seta ServiceImpl a likhi. etai Abstraction
+ */
 public interface StudentService {
 
+	/*
+	 * "Student" holo returnType. Ei method ekta Student object return korbe.
+	 * (Student student) "Student" holo DataType and "student" holo Parameter.
+	 * Ei method er majhe ekta student object asbe.
+	 * Mane ekta Student nebe and seta save korbe then saved Student return dibe.
+	 */
     Student saveStudent(Student student);
 
+    // onek gula Student return korte hobe tai List.
     List<Student> getAllStudents();
 }
